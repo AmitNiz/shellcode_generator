9a720a69e681edef52b1ff5d320f743461b81994
Amit Nizri <amitnzri@gmail.com>
2022-06-19 10:39:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ LINK_NAME=shcg
 
 echo "[+] Installing shellcode generator..\n"
 
-git clone https://github.com/AmitNiz/shellcode_generator $DIR_PATH && \
+(git clone https://github.com/AmitNiz/shellcode_generator $DIR_PATH || sudo git clone https://github.com/AmitNiz/shellcode_generator $DIR_PATH) && \
 ln -s $PATH/shellcode_generator.py $LINK_PATH/$LINK_NAME && \
 echo "\n[+] Done. Shellcode Generator is accessible as:\033[1m shcg \033[0m" || \
 echo "\n[!] Installation Failed."
