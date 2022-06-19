eb8490e2ed2cd1a6b17bc75180d0f4bdeedc151e
Amit <amit@Amits-MacBook-Pro.local>
2022-06-19 10:11:51 +0300

    update: installation script

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ LINK_PATH=/usr/local/bin
 LINK_NAME=shcg
 
 echo "[+] Installing shellcode generator..\n"
+
 git clone https://github.com/AmitNiz/shellcode_generator $DIR_PATH && \
-ln -s $PATH/shellcode_generator.py $LINK_PATH/$LINK_NAME
-echo "\n[+] Done. Shellcode Generator is accessible as:\033[1m shcg \033[0m"
+ln -s $PATH/shellcode_generator.py $LINK_PATH/$LINK_NAME && \
+echo "\n[+] Done. Shellcode Generator is accessible as:\033[1m shcg \033[0m" || \
+echo "\n[!] Installation Failed."
+
