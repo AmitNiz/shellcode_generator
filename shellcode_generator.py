d0fc4067b5ca78c15b68f792e9f2552f2b070dd6
Amit Nizri <amitnzri@gmail.com>
2022-06-19 11:15:41 +0300

    Update shellcode_generator.py

--- shellcode_generator.py
+++ shellcode_generator.py
@@ -23,7 +23,7 @@ banner = '''
   \_____|\___|_| |_|\___|_|  \__,_|\__\___/|_|   
                                                  
 
-  ** Build by Amit Nizri - https://github.com/AmitNiz **
+  ** https://github.com/AmitNiz/shellcode_generator.git **
 '''
 if len(sys.argv) == 1 or '-h' in sys.argv or '--help' in sys.argv:
 	print(banner)
